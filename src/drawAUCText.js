2c1e74e267391d54b7cd43072037a377959919e7
Micah Stubbs <micah.stubbs@gmail.com>
2016-07-12 13:05:55 -0700

    lint drawAUCText

--- src/drawAUCText.js
+++ src/drawAUCText.js
@@ -1,23 +1,23 @@
 export default function (svg, tpr, width, height, label, aucFormat, auc) {
   svg.append('g')
-    .attr('class', tpr + 'text')
+    .attr('class', `${tpr}text`)
     .style('opacity', 0)
     .attr('transform', `translate(${0.5 * width}, ${0.79 * height})`)
     .append('text')
       .text(label)
       .style({
-        'fill': 'white',
+        fill: 'white',
         'font-size': 18
       });
 
   svg.append('g')
-    .attr('class', tpr + 'text')
+    .attr('class', `${tpr}text`)
     .style('opacity', 0)
     .attr('transform', `translate(${0.5 * width}, ${0.84 * height})`)
     .append('text')
-      .text('AUC = ' + aucFormat(auc))
+      .text(`AUC = ${aucFormat(auc)}`)
       .style({
-        'fill': 'white',
+        fill: 'white',
         'font-size': 18
       });
 }
