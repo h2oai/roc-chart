4c42769d221f63f0e88ed3ff65af67b7195aa198
Micah Stubbs <micah.stubbs@gmail.com>
2016-07-12 13:00:26 -0700

    lint generatePoints

--- src/generatePoints.js
+++ src/generatePoints.js
@@ -1,5 +1,5 @@
 export default function (data, x, y) {
   const points = [];
-  data.forEach(d => { points.push([ Number(d[x]), Number(d[y]) ]) });
+  data.forEach(d => { points.push([Number(d[x]), Number(d[y])]); });
   return points;
 }
